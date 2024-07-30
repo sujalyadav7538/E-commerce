7dfc6e4cb10fd3384c52acc68cf30a67aa34e41c
sujal <ashutoshsingh2920@gmail.com>
2024-07-30 11:47:02 +0530

    Bugs

--- api/index.js
+++ api/index.js
@@ -60,10 +60,10 @@ app.use('/api/admin',adminRoute)
 
 
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.join(__dirname,'/client')));
 
 app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+  res.sendFile(path.join(__dirname,'client','index.html'))
 })
 
 app.use((err, req, res, next) => {
