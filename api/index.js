cbebbaee8582f7709db0b395f28e616e6b4361e0
sujal <ashutoshsingh2920@gmail.com>
2024-07-30 16:03:28 +0530

    index api

--- api/index.js
+++ api/index.js
@@ -1,18 +1,15 @@
-import express from "express";
-import DatabaseConnection from "./middlewares/database.js";
+import express from 'express';
+import path from 'path';
+import DatabaseConnection from './middlewares/database.js';
 import productRoute from './routes/product.route.js';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import userRoute from './routes/user.route.js';
 import adminRoute from './routes/admin.route.js';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import upload from './middlewares/multer.js';
 import uploadOnCloud from './utils/cloudinary.js';
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import  path  from 'path';
-
-const __dirname=path.resolve();
-
 
 dotenv.config({ path: "./.env" });
 
@@ -21,6 +18,7 @@ const app = express();
 // Connect to MongoDB Atlas
 await DatabaseConnection();
 
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -43,33 +41,30 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-
-app.get('/api/cookie-tester',(req,res,next)=>{
-    try{
-        if (req.cookies.access_token==undefined){
+app.get('/api/cookie-tester', (req, res, next) => {
+    try {
+        if (req.cookies.access_token == undefined) {
             return res.json("");
         }
-        res.json(req.cookies.access_token)
-
-    } catch(err){
+        res.json(req.cookies.access_token);
+    } catch (err) {
         next(err);
     }
-})
+});
+
 app.post('*/upload', upload.fields([{ name: 'product_image', maxCount: 6 }]), uploadOnCloud);
-
-app.use(express.static(path.join(__dirname,'/client/dist')));
-
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})
-
 
 app.use('/api/product', productRoute);
 app.use('/api/user', userRoute);
-app.use('/api/admin',adminRoute)
+app.use('/api/admin', adminRoute);
 
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-
+// Serve the index.html file for any unknown routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
@@ -80,7 +75,6 @@ app.use((err, req, res, next) => {
         message,
     });
 });
-
 
 app.listen(3000, () => {
     console.log('Listening on PORT 3000!');
