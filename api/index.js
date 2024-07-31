5cab09880c00d02ca9da9bed38f8368e45ab49c8
sujal <ashutoshsingh2920@gmail.com>
2024-07-31 10:27:32 +0530

    Path file

--- api/index.js
+++ api/index.js
@@ -63,7 +63,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Serve the index.html file for any unknown routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.use((err, req, res, next) => {
